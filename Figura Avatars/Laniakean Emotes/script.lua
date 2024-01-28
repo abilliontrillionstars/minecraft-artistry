1819d3eaf1727aad86a3ea43a9943b2d238e0328
Laniakea <billiontrillionstars@gmail.com>
2024-01-28 00:42:23 -0500

    Add Wheel Pages and Emotes
    - organized the emotes and hex commands into two subpages
    - refined the trident twirl emote & added sounds to it
    - made the test action to be a hex command-- resets the hex-sifted message

--- Figura Avatars/Laniakean Emotes/script.lua
+++ Figura Avatars/Laniakean Emotes/script.lua
@@ -1,24 +1,50 @@
 -- Auto generated script file --
 vanilla_model.PLAYER:setVisible(false)
+vanilla_model.LEFT_ELYTRA:setVisible(false)
+vanilla_model.RIGHT_ELYTRA:setVisible(false)
+
 
 local mainPage = action_wheel:newPage()
 action_wheel:setPage(mainPage)
+local hexPage = action_wheel:newPage()
+local emotePage = action_wheel:newPage()
 
-function pings.laniTestAction()
-    host:sendChatMessage("wah")
-end
+-- navigation functions --
+local action = mainPage:newAction()
+    :title("Hex Commands"):item("hexcasting:spruce_staff")
+    :hoverColor(0.164, 0.023, 0.247)
+    :onLeftClick(function() action_wheel:setPage(hexPage) end)
+local action = hexPage:newAction()
+    :title("Return to Main"):item("hexcasting:spruce_staff")
+    :onLeftClick(function() action_wheel:setPage(mainPage) end)
 
 local action = mainPage:newAction()
-    :title("Lani Test Action")
-    :item("minecraft:stick")
-    :hoverColor(0.274, 0.317, 0.086)
-    :onLeftClick(pings.laniTestAction)
+    :title("Emotes"):item("minecraft:trident")
+    :hoverColor(0.188, 0.309, 0.25)
+    :onLeftClick(function() action_wheel:setPage(emotePage) end)
+local action = emotePage:newAction()
+    :title("Return to Main"):item("minecraft:trident")
+    :onLeftClick(function() action_wheel:setPage(mainPage) end)
 
+-- HEX COMMANDS --
+function pings.clearHex()
+    host:sendChatMessage("!")
+    action_wheel:setPage(mainPage)
+end
+local action = hexPage:newAction()
+    :title("Clear Command"):item("hexcasting:slate")
+    :hoverColor(0.3, 0.3, 0.3):onLeftClick(pings.clearHex)
+
+
+
+
+
+-- EMOTES / VISUALS --
 function pings.laniTestAnim()
     animations.model.twirl_trident:play()
+    action_wheel:setPage(mainPage)
 end
-
-local action = mainPage:newAction()
+local action = emotePage:newAction()
     :title("Emote: Twirl Trident")
     :item("minecraft:trident")
     :hoverColor(0.274, 0.317, 0.086)
