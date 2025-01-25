a640ac5ab3e1971d0b8ef0f0d0e95e336b77003a
Laniakea <billiontrillionstars@gmail.com>
2025-01-25 02:17:33 -0500

    Virtuoso first batch of emote arcana

--- Figura Avatars/Virtuoso/arcana/brush-shift.lua
+++ Figura Avatars/Virtuoso/arcana/brush-shift.lua
@@ -20,7 +20,7 @@ end
 function events.item_render(item, mode)
     local id = item:getID()
     if brushSkins[id] then return brushSkins[id] end
-    if id:find("staff") then
+    if id:find("staff") and id:find("hexcasting") then
         if host:getScreen() == "at.petrak.hexcasting.client.gui.GuiSpellcasting" and BRUSHMODE=="WAND" then
             return models.iris.ItemBrush:setRot(-45,0,0)
         else
--- Figura Avatars/Virtuoso/arcana/chroma.lua
+++ Figura Avatars/Virtuoso/arcana/chroma.lua
@@ -16,10 +16,6 @@ testPage:newAction()
 testPage:newAction()
     :title("Test Brush-Staff"):item("hexcasting:staff/spruce")
     :onLeftClick(function() pings.ToggleBrushStaff() end)
-testPage:newAction()
-    :title("Test Staff Emotecast"):item("hexcasting:staff/cherry")
-    :onLeftClick(function() pings.playAnim("castStaffOrbit1") end)
-
 
 OrangePage:newAction()
     :title("Ignite Monsters"):item("minecraft:fire_charge")
@@ -29,9 +25,45 @@ OrangePage:newAction()
         pings.playAnim("castStaffOrbit1") 
     end)
 PinkPage:newAction()
-    :title("Heal Players"):item("minecraft:glistering_melon")
+    :title("Heal Players"):item("minecraft:glistering_melon_slice")
     :onLeftClick(function() 
         pings.ColorFX("pink")
         ArcanaString = "area.heal.player"
         pings.playAnim("castStaffOrbit1") 
+    end)
+
+YellowPage:newAction()
+    :title("Brush Shift: Radiance Staff"):item("spectrum:radiance_staff")
+    :onLeftClick(function() 
+        pings.ColorFX("yellow")
+        ArcanaString = "brush-shift.radiance staff,1"
+        pings.playAnim("castSpinRaiseWand") 
+    end)
+LimePage:newAction()
+    :title("Brush Shift: Nature's Staff"):item("spectrum:natures_staff")
+    :onLeftClick(function() 
+        pings.ColorFX("lime")
+        ArcanaString = "brush-shift.nature's staff,1"
+        pings.playAnim("castSpinRaiseWand") 
+    end)
+CyanPage:newAction()
+    :title("Brush Shift: Multitool"):item("spectrum:multitool")
+    :onLeftClick(function() 
+        pings.ColorFX("cyan")
+        ArcanaString = "brush-shift.multitool"
+        pings.playAnim("castSpinRaiseWand") 
+    end)
+
+testPage:newAction()
+    :title("Drawing <-> Painting"):item("spectrum:paintbrush")
+    :onLeftClick(function() 
+        pings.ColorFX("white")
+        if player:isLoaded() then
+            if player:getHeldItem():getID() == "spectrum:paintbrush" then
+                ArcanaString = "brush-shift.birch staff,1" 
+            else
+                ArcanaString = "brush-shift.paintbrush,1"
+            end
+        end
+        pings.playAnim("castSpinRaiseWand") 
     end)
