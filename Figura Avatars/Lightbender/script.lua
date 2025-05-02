b5aaee2dd428bfcbd2f6c2ac77ff87877e5cf500
Laniakea <billiontrillionstars@gmail.com>
2025-05-02 00:05:20 -0400

    add Lightbender utils

--- Figura Avatars/Lightbender/script.lua
+++ Figura Avatars/Lightbender/script.lua
@@ -1,7 +1,20 @@
--- Auto generated script file --
-
---hide vanilla model
 vanilla_model.PLAYER:setVisible(false)
-
---hide vanilla armor model
 vanilla_model.ARMOR:setVisible(false)
+
+local mainPage = action_wheel:newPage()
+action_wheel:setPage(mainPage)
+
+mainPage:newAction()
+    :title("Party Invite Vex"):item("minecraft:iron_sword")
+    :hoverColor(vectors.hexToRGB("#462451"))
+    :onLeftClick(function() host:sendChatMessage("/party invite Vexify27") end)
+mainPage:newAction()
+    :title("Housing Invite Vex"):item("minecraft:vex_spawn_egg")
+    :hoverColor(vectors.hexToRGB("#462451"))
+    :onLeftClick(function() host:sendChatMessage("/housing invite Vexify27") end)
+mainPage:newAction()
+    :title("Toggle Housing Edit"):item("minecraft:oak_log")
+    :hoverColor(vectors.hexToRGB("#462451"))
+    :onLeftClick(function() host:sendChatMessage("/housing edit") end)
+
+    
