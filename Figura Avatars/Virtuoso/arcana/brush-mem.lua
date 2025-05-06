8af4c7ee37844661190a9089ce5739cc95f69380
Laniakea <billiontrillionstars@gmail.com>
2025-05-05 23:03:48 -0400

    detect Virtuoso brush color nbt change & fix dash anim

--- Figura Avatars/Virtuoso/arcana/brush-mem.lua
+++ Figura Avatars/Virtuoso/arcana/brush-mem.lua
@@ -23,25 +23,56 @@ ArcanaCache = {
 }
 
 local heldColor
+local oldColor = "white"
+function events.tick()
+    if host:isHost() then
+        if player:getHeldItem():getID() ~= "spectrum:paintbrush" then return end
+
+        heldColor = player:getHeldItem():getTag()["Color"] -- e.g. "spectrum:black"
+        if not heldColor then return end
+        heldColor = string.sub(heldColor, heldColor:find(":")+1, -1) -- ->"black"
+
+        if oldColor ~= heldColor then
+            pings.ColorFX(heldColor)
+            oldColor = heldColor
+            pings.ColorBrush(heldColor)
+        end
+    end
+end
+
+function pings.ColorBrush(color)
+    for var=0,50 do
+        if player:isLoaded() and avatar:getRemainingParticles() then
+            particles:newParticle("spectrum:"..color.."_crafting", 
+              player:getPos()+vec(math.sin(var),0,math.cos(var)), 
+              vec(math.random(-50,50)/1000,0.01+math.random()/10,math.random(-50,50)/1000)
+            )
+        end
+    end
+    if player:isLoaded() then 
+        sounds:playSound("spectrum:spectrum.ui.button_click", player:getPos(), 1)
+        sounds:playSound("spectrum:crafting_ding", player:getPos(), 0.2, math.random(8,12)/10)
+    end
+end
+
+-- eat wasd presses while in the spectrum color selector screen
+function events.key_press(key, action)
+    if action==1 and (key==87 or key==65 or key==83 or key==68) 
+    and host:getScreen() == "de.dafuqs.spectrum.inventories.PaintbrushScreen" 
+        then return false end
+end
+
+
 quickcastKey = keybinds:newKeybind("Quickcast via Paintbrush", "key.mouse.middle", false)
 quickcastKey.press = function() 
-    if player:isLoaded() then
-        if player:getHeldItem():getID() ~= "spectrum:paintbrush" then
-            host:setActionbar("Hold your Paintbrush!")
-            return
-        end
-        heldColor = player:getHeldItem():getTag()["Color"] -- e.g. "spectrum:black"
-        if not heldColor then print("error: Paintbrush has no color!") return end
-        heldColor = string.sub(heldColor, heldColor:find(":")+1, -1) -- ->"black"
-        if ArcanaCache[heldColor].message ~= "" and ArcanaCache[heldColor].anim ~= "" then
-            SendArcana(ArcanaCache[heldColor].message, ArcanaCache[heldColor].anim, heldColor) 
-            -- special, swap between the slingInk's
-            if ArcanaCache[heldColor].anim == "castSlingInk1" then
-                ArcanaCache[heldColor].anim = "castSlingInk2"
-            elseif ArcanaCache[heldColor].anim == "castSlingInk2" then
-                ArcanaCache[heldColor].anim = "castSlingInk1" end
-        end 
-    end
+    if ArcanaCache[heldColor].message ~= "" and ArcanaCache[heldColor].anim ~= "" then
+        SendArcana(ArcanaCache[heldColor].message, ArcanaCache[heldColor].anim, heldColor) 
+        -- special, swap between the slingInk's
+        if ArcanaCache[heldColor].anim == "castSlingInk1" then
+            ArcanaCache[heldColor].anim = "castSlingInk2"
+        elseif ArcanaCache[heldColor].anim == "castSlingInk2" then
+            ArcanaCache[heldColor].anim = "castSlingInk1" end
+    end 
 end
 
 
--- Figura Avatars/Virtuoso/arcana/dexdash.lua
+++ Figura Avatars/Virtuoso/arcana/dexdash.lua
@@ -31,7 +31,7 @@ end
 function events.key_press(key, action, mod)
     --if action==1 then print(key) end
     -- w, a, s, d
-    if action==1 and (key==87 or key==65 or key==83 or key==68) then 
+    if action==1 and (key==87 or key==65 or key==83 or key==68) and not host:getScreen() then 
         if lastStep == key and sinceLastStep<5 then
             if lastStep==87 then
                 pings.playAnim("dashForward")
